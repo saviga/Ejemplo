103f06bd38da3f76afc8ca7c73d24ebb6123711a
saviga <saviga95@gmail.com>
2016-02-12 11:59:46 +0100

    Update DadoNombres.java

--- src/DadoNombres.java
+++ src/DadoNombres.java
@@ -10,5 +10,6 @@ public class DadoNombres {
         num= (int) (Math.random()*6);
         System.out.print(num);
         System.out.print(Nombres[num]);
+        System.out.println("Hola creo que esta muy bien")
     }
 }
